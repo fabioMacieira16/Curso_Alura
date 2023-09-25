24d43c78d3807dac18b54de1ba05eb078948c48d
AzureAD\FábioVitorMacieirade <fabio.macieira@lincros.com>
2023-09-25 18:53:20 -0300

    Commit initial

---
+++ chatgpt-javascript/server.js
@@ -0,0 +1,8 @@
+// process.title = 'pong';
+//   1. var args = process.argv,
+//   2.   port = args[2] || 3000,
+//   3.   webServer = require('./server');
+//   4.
+//   5. webServer.listen(port, function() {
+//   6.   console.log('Server started at port ' + port);
+//   7. });
