3c744ee22253bfc7084dde47b9d4d2c7444cf1ab
AzureAD\FábioVitorMacieirade <fabio.macieira@lincros.com>
2023-09-28 09:48:54 -0300

    configurando o server

--- chatgpt-javascript/server.js
+++ chatgpt-javascript/server.js
@@ -1,8 +1,21 @@
-// process.title = 'pong';
-//   1. var args = process.argv,
-//   2.   port = args[2] || 3000,
-//   3.   webServer = require('./server');
-//   4.
-//   5. webServer.listen(port, function() {
-//   6.   console.log('Server started at port ' + port);
-//   7. });
+//criando um serve json
+const express = require('express');
+const livereload = require('livereload');
+
+const app = express();
+const serve = livereload.createServer();
+
+//definir a pastar raiz do projeto
+const publicDir = __dirname;
+
+//Configurar o serviddor para eviar arquivo estaticos
+app.use(express.static(publicDir));
+
+//moninotar os arquivos Html, css e js e recarregar a pagina quando nescessario
+serve.watch(publicDir);
+
+//iniciar o servidor na porta 3000
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor de desenvolvimento esta rodando na porta ${port}`);
+}); 
