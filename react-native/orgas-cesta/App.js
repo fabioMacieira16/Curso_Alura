ba2b1a46d2050c6d3d1c3488e562d369f8341223
AzureAD\FábioVitorMacieirade <fabio.macieira@lincros.com>
2023-11-01 13:36:16 -0300

    criando a imagem e titulo da tela

--- react-native/orgas-cesta/App.js
+++ react-native/orgas-cesta/App.js
@@ -1,24 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Cesta from '../src/telas/Cesta';
+import React from 'react';
+import { StatusBar,  SafeAreaView } from 'expo-status-bar';
+
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar /> 
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>    
   );
 }
-
-constinuar da aula
-https://cursos.alura.com.br/course/react-native-comecando-zero/task/97005
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- react-native/orgas-cesta/src/telas/Cesta.js
+++ react-native/orgas-cesta/src/telas/Cesta.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Image, Dimensions,Text, View } from 'react-native';
+
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return (
-            <Text>Holá Mundo "Cestas"!</Text>
-    );
+        <>
+            <Image source={topo} style={estilo.topo} />
+            <Text>Detalhes da cesta</Text>
+
+            <View>
+                <Text>Ce</Text>
+            </View>
+        </>
+    )
 }
+
+const estilo = StyleSheet.create({
+    topo: {
+        width: '100%',
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width: '100%',
+        position: 'absolute',
+        textAlign: 'center',
+        fontSize: 16,
+        lineHeight: 16,
+        color: 'white',
+        fontWeight: 'bold',
+        padding: 16,
+    }
+})
