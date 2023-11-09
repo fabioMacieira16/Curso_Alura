7cc1d0ac679d9f7de26c7315e5c57f62317989eb
AzureAD\FábioVitorMacieirade <fabio.macieira@lincros.com>
2023-11-09 18:30:29 -0300

    correcao do componet cesta

--- react-native/orgas-cesta/App.js
+++ react-native/orgas-cesta/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar,  SafeAreaView } from 'expo-status-bar';
+import {StatusBar, SafeAreaView } from 'react-native';
 
 import Cesta from './src/telas/Cesta';
 
@@ -11,3 +11,4 @@ export default function App() {
     </SafeAreaView>    
   );
 }
+
--- react-native/orgas-cesta/src/telas/Cesta.js
+++ react-native/orgas-cesta/src/telas/Cesta.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions,Text, View } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,10 +10,16 @@ export default function Cesta() {
     return (
         <>
             <Image source={topo} style={estilo.topo} />
-            <Text>Detalhes da cesta</Text>
-
-            <View>
-                <Text>Ce</Text>
+            <Text style={estilo.titulo} >Detalhe da cesta</Text>
+            <View style={estilo.cesta}>
+                <Text style={estilo.nome}>Cesta de Verdura</Text>
+                <View style={estilo.fazenda}>
+                    <Image source={logo} style={estilo.imageFazenda} />
+                    <Text style={estilo.nomeFazenda}>Jenny Jack Farm</Text>
+                </View>
+                <Text style={estilo.descricao}>Uma Cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+                <Text style={estilo.preco}>R$ 40,00</Text>
+                <Text style={estilo.quantidade}>quantidade</Text>
             </View>
         </>
     )
@@ -28,9 +35,50 @@ const estilo = StyleSheet.create({
         position: 'absolute',
         textAlign: 'center',
         fontSize: 16,
-        lineHeight: 16,
+        lineHeight: 156,
         color: 'white',
         fontWeight: 'bold',
         padding: 16,
-    }
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    fazenda: {
+        flexDirection: 'row',
+        paddingVertical: 12,
+    },
+    imageFazenda: {
+        width: 32,
+        height: 32,
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold',
+    },
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+    },
+    quantidade: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26,
+        marginTop: 24,
+    },
 })
